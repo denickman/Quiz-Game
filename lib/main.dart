3fb6e55069a58628503963fa5abcf5a35a319617
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-18 10:41:33 +0300

    Add StartScreen widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,21 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: BoxDecoration(gradient: 
+          LinearGradient(colors: 
+          [
+              Colors.deepPurple.shade400,
+              Colors.deepPurple.shade300,
+              Colors.deepPurple.shade200,
+              Colors.deepPurple.shade100
+          ],  
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          ),
+          ),
+          child: const StartScreen()
+          ),
         ),
       ),
   );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-
   // если в классе нет const конструктора, ты вообще не сможешь написать const StartScreen().
   const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return const Text('Start screen');
-  }
+    // Center по умолчанию занимает всё доступное пространство (то есть весь размер родителя),
+    // а уже внутри центрует своего потомка (Text).
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize
+            .min, // “Не растягивайся на весь экран по вертикали, а занимай только столько места, сколько нужно детям (Image)”.
 
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+
+          // Image.asset() нельзя сделать const. он создаёт Image динамически
+          const SizedBox(height: 80),
+
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors
+                  .white, // Colors.white — это готовая константа в классе Colors.
+              // color: Colors.amber.shade800,
+              /*
+          Colors.amber — это MaterialColor, который внутри хранит карту оттенков (Map<int, Color>).
+          Когда ты пишешь .shade800, это геттер, который возвращает значение из этой карты во время 
+          выполнения (runtime).     
+          Colors.amber.shade800 нельзя вычислить на этапе компиляции, потому что это не const
+          */
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
 }
